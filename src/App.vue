bd0f090185831c9ea97bfa9538c3090b6e2f8f51
jack <you@example.com>
2021-04-01 17:09:25 +0300

    Add NewQuote comopnent.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
+    <new-quote @quoteAdded ="newQuote"></new-quote>
     <quote-grid :quotes="quotes"></quote-grid>
   </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+
 export default {
   data(){
     return{
@@ -17,7 +20,15 @@ export default {
   },
   components:{
     'quoteGrid':QuoteGrid,
+    'newQuote':NewQuote,
+  },
+  methods:{
+    newQuote(quote){
+      this.quotes.push(quote);
+    }
+    
   }
+
   
 }
 </script>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row justify-content-center">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 col-lg-5 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-outline-success" @click.prevent="createNew"> New Quote </button>
+            </div>
+        </form>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            'quote':''
+        };
+    },
+    methods:{
+        createNew(){
+           this.$emit('quoteAdded', this.quote);
+           this.quote = '';
+
+
+        },
+    }
+    
+} 
+</script>
+
+<style>
+    
+</style>>
