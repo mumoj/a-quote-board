e1cb17a4332387b1ef4e696a85d119f17d9bd164
jack <you@example.com>
2021-04-12 16:35:11 +0300

    Add the progress bar component.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,54 @@
 <template>
   <div class="container">
-    <new-quote @quoteAdded ="newQuote"></new-quote>
+    <quotes-progress-bar :numberOfQuotes="numberOfQuotes()" :maxQuotes="maxQuotes"></quotes-progress-bar>
+    <new-quote @quoteAdded="newQuote"></new-quote>
     <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+
+    <div class="alert alert-info text-center mb-4" role="alert">
+      Click a quote to delete it!
+    </div>
+
   </div>
 </template>
 
 <script>
-import QuoteGrid from './components/QuoteGrid.vue';
-import NewQuote from './components/NewQuote.vue';
 
-export default {
+import NewQuote from './components/NewQuote.vue';
+import QuotesProgressBar from './components/QuotesProgressBar.vue';
+import QuoteGrid from './components/QuoteGrid.vue';
+
+export default { 
   data(){
     return{
         quotes:[
           'Test Quote',
         ],
-        maxQuotes:10
+        maxQuotes:10,
     }
   },
   components:{
     'quoteGrid':QuoteGrid,
     'newQuote':NewQuote,
+    'quotesProgressBar':QuotesProgressBar,
   },
   methods:{
-    newQuote(quote){
-      this.quotes.push(quote);
+    newQuote(quote,event){
+      if (this.quotes.length < 10) {
+        this.quotes.push(quote);
+      }
+      else{
+        event.stopPropagation();   
+      }
     },
+
     deleteQuote(index){
       this.quotes.splice(index,1);
     },
-    
-  }
 
-  
+    numberOfQuotes(){
+      return this.quotes.length
+    }
+  }
 }
 </script>
 
---
+++ src/components/QuotesProgressBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="progress-bar-striped bg-info text-center w-10 mt-3" role="progressbar" 
+    :style="{width:numberOfQuotes/maxQuotes * 100 + '%'}">
+        {{numberOfQuotes}} / {{maxQuotes}}
+    </div>
+</template>
+
+<script>
+export default {
+    props:['numberOfQuotes', 'maxQuotes' ],
+}
+</script>
+
+<style>
+    
+</style>
